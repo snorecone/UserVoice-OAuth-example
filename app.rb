4c5cd2c7992543fe9f45d315dfeb3cc94d0eb03c
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-09-25 12:45:43 -0700

    Cleanup the UI from unnecessary content

--- app.rb
+++ app.rb
@@ -16,22 +16,28 @@ class MyApp < Sinatra::Base
                                 :protocol => config['protocol'],
                                 :callback => 'http://localhost:4567/')
   end
+  def current_user
+    if @current_user.nil? && session[:access_token]
+      @current_user = session[:access_token].get('/api/v1/users/current.json')['user']
+    end
+    return @current_user
+  end
 
   def current_sso_token
-    unless @current_user.nil?
-      user = { :email => @current_user['email'], :trusted => true }
+    unless current_user.nil?
+      user = { :email => current_user['email'], :trusted => true }
       return UserVoice.generate_sso_token(config['subdomain_name'], config['sso_key'], user, 300)
     end
   end
   
   before do
     if params[:oauth_verifier]
-      session[:access_token] = client.login_with_verifier(params[:oauth_verifier])
+      begin
+        session[:access_token] = client.login_with_verifier(params[:oauth_verifier])
+      rescue UserVoice::Unauthorized
+        # false/old verifier
+      end
       redirect to('/')
-    end
-
-    if session[:access_token]
-      @current_user = session[:access_token].get('/api/v1/users/current.json')['user']
     end
   end
   
@@ -52,6 +58,11 @@ class MyApp < Sinatra::Base
     rescue Exception => e
       e.backtrace.join("\n")
     end
+  end
+
+  get '/logout' do
+    session[:access_token] = session[:request_token] = @current_user = nil
+    redirect to('/')
   end
 
   get '/authorize-uservoice' do
